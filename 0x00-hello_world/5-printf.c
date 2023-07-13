1f8d2f9e8c414004294722618f7ae5f984c876a4
lolosaisa <lwamaitha3924@gmail.com>
2023-07-13 07:07:49 -0700

    correction

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -8,9 +8,10 @@
  *
  * Return: 0 (SUCCESS)
  */
+
 int main(void)
 {
 	
-	printf("with proper grammar, but the outcome is a piece of art, \n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
