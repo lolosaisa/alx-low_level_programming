2d6b53cb5efbcbb1fbdf92c8d76137a79404867e
lolosaisa <lwamaitha3924@gmail.com>
2023-07-13 11:21:21 -0700

    betty coding style

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
 
 /**
- * main - Entry point of the program
+ * main- Entry point
  *
- * Decription: A program that prints the size of types
+ * Description: Program
  *
- * Return: Always 0 (Success)
+ * Return:0
  */
 int main(void)
 {
-    printf("Size of a char: %lu byte(s)\n", sizeof(char));
-    printf("Size of an int: %lu byte(s)\n", sizeof(int));
-    printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
-    printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
-    printf("Size of a float: %lu byte(s)\n", sizeof(float));
+	printf("Size of char: %zu bytes\n", sizeof(char));
+	printf("Size of short: %zu bytes\n", sizeof(short));
+	printf("Size of int: %zu bytes\n", sizeof(int));
+	printf("Size of long: %zu bytes\n", sizeof(long));
+	printf("Size of long long: %zu bytes\n", sizeof(long long));
+	printf("Size of float: %zu bytes\n", sizeof(float));
+	printf("Size of double: %zu bytes\n", sizeof(double));
+	printf("Size of long double: %zu bytes\n", sizeof(long double));
 
-    return 0;
+	return (0);
 }
-
