60cdaf4a7d52f41cfa9e03949533945844bda92f
lolosaisa <lwamaitha3924@gmail.com>
2023-07-16 05:45:14 -0700

    second commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - Entry point for my code
+ *
+ * Description: Prints out the single digit numbera
+ * of base ten
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+		putchar (num);
+	return (0);
+}
