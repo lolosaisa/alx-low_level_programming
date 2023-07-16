fc5d8faa34e336290f1bf9529e28eb7970dd4199
lolosaisa <lwamaitha3924@gmail.com>
2023-07-16 09:51:41 -0700

    second commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,20 +11,20 @@ int main(void)
 	int num1;
 	int num2;
 
-	for (num1 = 0; num1 <= 99; num1++)
+	for (num1 = 0; num1 < 99; num1++)
 	{
 		for (num2 = 0; num2 <= 99; num2++)
 		{
-			int tens1 = num1 / 10;
-			int ones1 = num1 % 10;
-			int tens2 = num2 / 10;
-			int ones2 = num2 % 10;
+			int t1 = num1 / 10;
+			int o1 = num1 % 10;
+			int t2 = num2 / 10;
+			int o2 = num2 % 10;
 
-			putchar(tens1 + '0');
-			putchar(ones1 + '0');
+			putchar(t1 + '0');
+			putchar(o1 + '0');
 			putchar(' ');
-			putchar(tens2 + '0');
-			putchar(ones2 + '0');
+			putchar(t2 + '0');
+			putchar(o2 + '0');
 
 			if (num1 != 99 || num2 != 99)
 			{
