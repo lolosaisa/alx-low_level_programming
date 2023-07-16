c3d05c07bfd88ff9d6b994b95c3a7f09cf37187d
lolosaisa <lwamaitha3924@gmail.com>
2023-07-16 08:14:48 -0700

    first commit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Description: Prints possible combination of two digits
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int num1;
+	int num2;
+
+	for (num1 = 0; num1 <= 9; num1++)
+	{
+		for (num2 = 0; num2 <= 9; num2++)
+		{
+			if (num1 != num2 && num1 > num2)
+			{
+				putchar ('0' + num1);
+				putchar ('0' + num2);
+
+				if (num1 != 8 || num2 != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
