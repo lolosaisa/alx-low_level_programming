6d6de544bc6a58bb8ca7519f394bc9d5ad35ff44
lolosaisa <lwamaitha3924@gmail.com>
2023-07-16 12:04:04 -0700

    changed the whole code

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,32 +8,28 @@
  */
 int main(void)
 {
-	int num1;
-	int num2;
+	int a;
+	int b;
 
-	for (num1 = 0; num1 < 99; num1++)
+	for (a = 0; a < 100; a++)
 	{
-		for (num2 = 0; num2 <= 99; num2++)
+		for (b = 0; b < 100; b++)
 		{
-			int t1 = num1 / 10;
-			int o1 = num1 % 10;
-			int t2 = num2 / 10;
-			int o2 = num2 % 10;
-
-			putchar(t1 + '0');
-			putchar(o1 + '0');
-			putchar(' ');
-			putchar(t2 + '0');
-			putchar(o2 + '0');
-
-			if (num1 != 99 || num2 != 99 || num1 < num2)
+			if (a < b)
 			{
-				putchar(',');
+				putchar((a / 10) + 48);
+				putchar((a % 10) + 48);
 				putchar(' ');
+				putchar((b / 10) + 48);
+				putchar((b % 10) + 48);
+				if (a != 98 || b != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
 	putchar('\n');
-
 	return (0);
 }
