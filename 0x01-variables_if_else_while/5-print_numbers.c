cc6b4420a9fcc8c7805ab090a6246ab6cbedcdae
lolosaisa <lwamaitha3924@gmail.com>
2023-07-16 05:33:42 -0700

    First commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - Entry point for my code
+ *
+ * Description: Program that prints single Digits
+ * of base ten
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+		putchar(num);
+	putchar('\n');
+
+	return (0);
+}
