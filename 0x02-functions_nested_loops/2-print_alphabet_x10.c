f46d5628e3d5dc0ac488b3c5a849a6126323d029
lolosaisa <lwamaitha3924@gmail.com>
2023-07-18 05:44:27 -0700

    first commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Prints alphabet ten times
+ *
+ * Description: Alphabet is printed ten times in lowercase
+ *
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+	int m;
+	int n;
+
+	for (m = 0; m <= 10; m++)
+	{
+		for (n = 97; n <= 122; n++)
+		{
+			_putchar(n);
+		}
+		_putchar('\n');
+	}
+}
