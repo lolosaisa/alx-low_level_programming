77f4ed939e20315e987f16dd65e88a969fa7ff67
lolosaisa <lwamaitha3924@gmail.com>
2023-07-18 04:54:43 -0700

    first commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.c"
+/**
+ * main - Entry point for my program
+ *
+ * Description: Calling function print
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	_putchar('\n');
+	return (0);
+}
