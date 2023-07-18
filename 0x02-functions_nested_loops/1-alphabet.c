a657934c2f77da6d70dcd87dbccc289fdf3c10a5
lolosaisa <lwamaitha3924@gmail.com>
2023-07-18 05:23:30 -0700

    first commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet - prints alphabet in lowercase
+ *
+ * Description: Program that prints the alphabet
+ * in  lowecase
+ *
+ * Retun: 0
+ */
+void print_alphabet(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		_putchar('a');
+	}
+	_putchar('\n');
+}
