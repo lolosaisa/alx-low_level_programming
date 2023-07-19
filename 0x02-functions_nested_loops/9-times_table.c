1ed269e641b5bb19fe2ee272b9c1ccf5f71c8122
lolosaisa <lwamaitha3924@gmail.com>
2023-07-19 03:32:41 -0700

    changed the code

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,22 +7,27 @@
  */
 void times_table(void)
 {
-	int m, n, o, p, q;
+	int m, n, o, p;
 
-	for (m = 0; m <= 9; m++)
+	for (m = 0; m <= 2; m++)
 	{
 		for (n = 0; n <= 9; n++)
 		{
-			o = m * n;
-			if (o > 9)
+			if ((m <= 1 && n <= 9) || (m <=2 && n <= 3))
 			{
-				p = o % 10;
-				q = (o - p) / 10;
-				_putchar(44);
-				_putchar(32);
+				for (o = 0; o <= 5; o++)
+				{
+					  for (p = 0; p <= 9; p++)
+					  {
+						  _putchar(m + '0');
+						  _putchar(n + '0');
+						  _putchar(58);
+						  _putchar(o + '0');
+						  _putchar(p + '0');
+						  _putchar('\n');
+					  }
+				}
 			}
-			_putchar(o + '0');
 		}
 	}
-	_putchar('\n');
 }
