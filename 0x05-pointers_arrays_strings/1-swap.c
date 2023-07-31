6a955919293f3ed486493d8aebe6d2299ef9112c
lolosaisa <lwamaitha3924@gmail.com>
2023-07-31 11:19:31 -0700

    second commit

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,11 +1,8 @@
 #include "main.h"
 /**
  * swap_int - Function to swap value of integers
- * @swap: Swaps the integers
- * @change: used to change value of integer
  * @a: swaps and store address of b
  * @b: swaps and stores address of a
- * 
  * Return: 0
  */
 void swap_int(int *a, int *b)
