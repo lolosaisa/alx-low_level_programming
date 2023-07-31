94a3a662b8f551d4c021338c627f3ad375792781
lolosaisa <lwamaitha3924@gmail.com>
2023-07-31 10:47:19 -0700

    first commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - Function that resets value of ponter n
+ * @n: Pointer name
+ * Return: 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Function that points the pointer to int
+ * @n: Updated 98 value
+ * Return: Always 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
