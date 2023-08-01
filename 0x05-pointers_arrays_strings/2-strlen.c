0c6c08af1834032c9c9d34bce556b2ecfc812228
lolosaisa <lwamaitha3924@gmail.com>
2023-08-01 02:37:12 -0700

    first commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - Function that calculates the length of the string
+ * @*s: Pointer that points to the first character
+ * of the string
+ * Return: Length of the sting
+ */
+int _strlen(char *s)
+{
+	int string_length = 0;
+
+	while (s[string_length])
+		string_length++;
+	return (string_length);
+}
