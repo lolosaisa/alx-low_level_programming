fda150cb02400423cb4ace7a99f80bb8841c82a1
lolosaisa <lwamaitha3924@gmail.com>
2023-08-09 03:01:44 -0700

    first commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point for progran
+ * @argc: number of things you enter
+ * @argv: array of strings
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+(void)argv;
+printf("%d\n", argc - 1);
+
+return (0);
+}
