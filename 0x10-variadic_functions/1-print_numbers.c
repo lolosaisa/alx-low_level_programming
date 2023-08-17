b6d23a21cb290e7d2401fe1d5180058ad94155a6
lolosaisa <lwamaitha3924@gmail.com>
2023-08-17 14:02:41 -0700

    fist commit

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,30 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints the numbers
+ * @separator: string between numbers
+ * @n: number of ints to be passed to function
+ *
+ * Return: no return
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list list;
+
+	va_start(list, n);
+
+	for (i = 0; i < n; i++)
+	{
+		if (!separator)
+			printf("%d", va_arg(list, int));
+		else if (separator && i == 0)
+			printf("%d", va_arg(list, int));
+		else
+			printf("%s%d", separator, va_arg(list, int));
+	}
+
+	va_end(list);
+
+	printf("\n");
+}
