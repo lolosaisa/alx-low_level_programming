73c1347e458be633439baedb16555ddfee4cf118
lolosaisa <lwamaitha3924@gmail.com>
2023-08-30 03:46:45 -0700

    creates an array of ints

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * array_range - Function Creates an array of integers
+ * @min: smallest array
+ * @max: largest array
+ *
+ * Return: 0 if successful
+ */
+int *array_range(int min, int max)
+{
+	int size;
+	int *arr;
+	int i;
+
+	if (min > max)
+		return (NULL);
+	size = max - min + 1;
+	arr = (int *)malloc(size * sizeof(int));
+	if (arr == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+	{
+		arr[i] = min + i;
+	}
+	return (arr);
+}
+
