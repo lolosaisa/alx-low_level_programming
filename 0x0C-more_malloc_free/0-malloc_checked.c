21e5eddb9f07d98fba2334dc2c70d1002e894e05
lolosaisa <lwamaitha3924@gmail.com>
2023-08-30 03:13:28 -0700

    allocate memory

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * malloc_checked - Function allocates mem through malloc
+ * @b: size of the memory
+ *
+ * Return: 0 is successful
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+	if (p == NULL)
+	{
+		exit(98);
+	}
+	return (p);
+}
