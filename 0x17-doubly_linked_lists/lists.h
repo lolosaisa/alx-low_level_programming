b3bc28c93e3366c8b40c15328db6aaac408850e1
lolosaisa <lwamaitha3924@gmail.com>
2024-04-02 15:40:49 -0700

    changed the header file

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -6,28 +6,28 @@
 
 /**
  * struct dlistint_s - doubly linked list
- * @n: an integer
+ * @n: integer
  * @prev: points to the previous node
- * @next: variable points to the next node
+ * @next: points to the next node
  *
- * Description: Holberton project for doubly
- * linked list node structure
+ * Description: doubly linked list node structure
+ * for Holberton project
  */
 typedef struct dlistint_s
 {
 	int n;
 	struct dlistint_s *prev;
-	struct dlistint_S *next;
-}dlistint_t;
+	struct dlistint_s *next;
+} dlistint_t;
 
-size_t dlistint_len(const dlistint_t *h);
-size_t print_dlistint(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
-void free_dlistint(dlistint_t *head);
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
+size_t print_dlistint(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h);
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
-int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
+void free_dlistint(dlistint_t *head);
 
 #endif
